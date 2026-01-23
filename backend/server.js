a7ec84dbc7c5432920efccac1cdcc2cfce914920
kuba <kubamaciejewski9@gmail.com>
2026-01-23 14:41:16 +0100

    port

--- backend/server.js
+++ backend/server.js
@@ -46,4 +46,5 @@ app.post('/pins/verify', async (req, res) => {
   }
 })
 
-app.listen(3000, () => console.log('API on :3000'))
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`API on :${PORT}`))
